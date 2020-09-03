605729f9e4835579394e9fadde55ff9363b609a0
mei15 <marimarpubg@gmail.com>
2020-09-03 09:38:24 -0700

    edit api

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -17,7 +17,7 @@ class UserController extends Controller
     {
         if (Auth::attempt(['email' => request('email'), 'password' => request('password')])) {
             $user = Auth::user();
-            $success['token'] = $user->createToken('appToken')->accessToken;
+            $success['token'] = $user->createToken('Sikap')->accessToken;
             //After successfull authentication, notice how I return json parameters
             return response()->json([
                 'success' => true,
