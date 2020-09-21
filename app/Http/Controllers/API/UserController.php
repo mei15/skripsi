cfd1b4f6457c9d92cd36ff6a9ed7f4f4f9bed4a3
mei15 <marimarpubg@gmail.com>
2020-09-21 09:12:19 +0700

    edit

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -19,32 +19,47 @@ class UserController extends Controller
      * @param Request $request
      * @return \Illuminate\Http\JsonResponse
      */
-    public $successStatus = 200;
-
-    public function login(){
-        if(Auth::attempt(['email' => request('email'), 'password' => request('password')])){
-            $user = Auth::user();
-            $success['token'] =  $user->createToken('nApp')->accessToken;
-            return response()->json(['success' => $success], $this->successStatus);
+    public function login()
+        {
+            if (Auth::attempt(['email' => request('email'), 'password' => request('password')])) {
+                $user = Auth::user();
+                $success['token'] = $user->createToken('appToken')->accessToken;
+               //After successfull authentication, notice how I return json parameters
+                return response()->json([
+                  'success' => true,
+                  'token' => $success,
+                  'user' => $user
+              ]);
+            } else {
+           //if authentication is unsuccessfull, notice how I return json parameters
+              return response()->json([
+                'success' => false,
+                'message' => 'Invalid Email or Password',
+            ], 401);
+            }
         }
-        else{
-            return response()->json(['error'=>'Unauthorised'], 401);
-        }
-    }
 
-    public function logout(Request $request)
+         /**
+     * Logout api.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    
+    public function logout(Request $res)
     {
-        $logout = $request->user()->token()->revoke();
-        if($logout){
-            return response()->json([
-                'message' => 'Successfully logged out'
-            ]);
-        }
-    }
+      if (Auth::user()) {
+        $user = Auth::user()->token();
+        $user->revoke();
 
-    public function details()
-    {
-        $user = Auth::user();
-        return response()->json(['success' => $user], $this->successStatus);
-    }
+        return response()->json([
+          'success' => true,
+          'message' => 'Logout successfully'
+      ]);
+      }else {
+        return response()->json([
+          'success' => false,
+          'message' => 'Unable to Logout'
+        ]);
+      }
+     }
 }
