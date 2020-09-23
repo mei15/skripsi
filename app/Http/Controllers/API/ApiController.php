6cdc934ce329a05a24eca1618e5fc57df5d94489
mei15 <marimarpubg@gmail.com>
2020-09-23 21:12:08 +0700

    tambah:api

---
+++ app/Http/Controllers/API/ApiController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use DB;
+use App\Dosen;
+use App\Mahasiswa;
+use App\Konsultasi;
+use Illuminate\Http\Request;
+
+
+class ApiController extends Controller
+{
+    public function index()
+    {
+        $dosens = Dosen::all();
+        return response()->json( [$dosens->toArray()] , 200);
+    }
+
+    public function mhs()
+    {
+        $mahasiswa = Mahasiswa::all();
+        return response()->json( [$mahasiswa->toArray()] , 200);
+    }
+
+    public function judul()
+    {
+        $konsultasi = Konsultasi::all()->judul;
+        return response()->json( [$konsultasi->toArray()] , 200);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -23,26 +23,12 @@ Route::group(['middleware' => 'auth:api'], function(){
     Route::get('/logout', 'API\UserController@logout');
     Route::get('/details', 'API\UserController@detail');
     Route::resource('/konsultasi', 'API\KonsultasiApiController');
+
 }); 
-// Route::prefix('auth')->group(function () {
-//     Route::post('/login', 'Api\LoginApiController@login');
-//     Route::get('/logout', 'Api\LoginApiController@logout')->middleware('jwt.verify');
-//     Route::get('/me', 'Api\LoginApiController@me')->middleware('jwt.verify');
-// });
 
-// Route::middleware('jwt.verify')->group(function () {
-//     Route::get('/semuadosen', 'Api\DosenApiController@index');
-// });
-
-// Route::middleware('jwt.verify')->group(function () {
-//     Route::get('/semuamahasiswa', 'Api\MahasiswaApiController@index');
-// });
-
-// Route::middleware('jwt.verify')->group(function () {
-//     Route::resource('/konsultasi', 'Api\KonsultasiApiController');
-// });
-
-//Untuk pesan error, kalo route diatas gk ada yg sama A.K.A. Error Handling
+Route::get('/dosen','API\ApiController@index');
+Route::get('/mahasiswa','API\ApiController@mahasiswa');
+Route::get('/judul', 'API\ApiController@judul');
 
 // Route::fallback(function () {
 //     return response()->json([
