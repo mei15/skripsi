e43d88da34797883ec209dc1370ac7a06396b0d5
mei15 <marimarpubg@gmail.com>
2020-10-15 06:59:49 +0700

    edit:konsultasi api

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -50,13 +50,7 @@
                                     </a>
                                     <div class="dropdown-menu dropdown-menu-right profile-dropdown ">
                                         <!-- item-->
-                                        <form method="POST" action="{{route('profile.edit')}}">
-                                            @csrf
-                                            <button type="submit" class="d-none" id='edit'></button>
-                                            <a href="{{ route('profile.edit') }}" class="dropdown-item"><i class="mdi mdi-account-settings-variant"></i>
-                                                Ubah Profil
-                                            </a>
-                                        </form>
+                                       
                                         <form method="POST" action="{{route('logout')}}">
                                             @csrf
                                             <button type="submit" class="d-none" id='logout'></button>
