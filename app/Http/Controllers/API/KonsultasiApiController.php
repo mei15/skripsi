772b61231f6bf1cd7f675bf3c5187d9cca2c696f
mei15 <marimarpubg@gmail.com>
2020-10-28 09:51:23 +0700

    edit

--- app/Http/Controllers/API/KonsultasiApiController.php
+++ app/Http/Controllers/API/KonsultasiApiController.php
@@ -42,7 +42,7 @@ class KonsultasiApiController extends Controller
     public function store(Request $request)
     {
         $user = auth()->user()->userable;
-        $dosens = Dosen::all();
+        
        
 
         $request->validate([
@@ -57,7 +57,7 @@ class KonsultasiApiController extends Controller
         $konsultasi->keterangan = $request->keterangan;
         $konsultasi->tanggal = $request->tanggal;
         $konsultasi->mahasiswa_id = $user->id;
-        $konsultasi->dosen_id = $dosens->$request->dosen;
+        $konsultasi->dosen_id = $request->dosen;
         $konsultasi->save();
         
             return response()->json(
