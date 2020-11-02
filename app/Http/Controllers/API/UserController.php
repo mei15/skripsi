b460d5b26e24ed9dded83a4c3af6d4538e5cb4ed
mei15 <marimarpubg@gmail.com>
2020-11-03 06:00:02 +0700

    ubah:api

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -48,7 +48,6 @@ class UserController extends Controller
                   'success' => true,
                   'token' => $success,
                   'user' => $user,
-                  'data' => auth()->user()->userable,
                   'konsultasi' => auth()->user()->userable->Konsultasi,
                   
               ]);
