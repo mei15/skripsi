ab03a9810d3f80ee37f00eff3cffaba9e161c8cc
mei15 <marimarpubg@gmail.com>
2020-11-02 07:01:59 +0700

    edit

--- app/Http/Controllers/API/KonsultasiApiController.php
+++ app/Http/Controllers/API/KonsultasiApiController.php
@@ -14,10 +14,10 @@ class KonsultasiApiController extends Controller
         // $user = auth()->user()->userable;
         $konsultasi = auth()->user()->userable->Konsultasi;
 
-        return response()->json(
+        return response()->json([
            
             $konsultasi
-        ,200);
+        ],200);
     }
 
     public function show($id)
